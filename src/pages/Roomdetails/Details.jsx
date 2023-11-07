2f1992a5419e61868ebcadf16d3d6b3b0dab20a5
vivek9985 <vivekanando363@gmail.com>
2023-11-07 18:19:29 +0600

    added room info in room details file

--- src/pages/Roomdetails/Details.jsx
+++ src/pages/Roomdetails/Details.jsx
@@ -1,17 +1,27 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import star from "../../assets/star.png";
+import { useContext } from "react";
+import { AuthContext } from "../../Authprovider/Authprovider";
 
 const Details = () => {
+  const { user } = useContext(AuthContext);
   const data = useLoaderData();
-  console.log(data);
   return (
     <section className="w-[85%] mx-auto py-16 mb-28">
-      <div className="grid grid-cols-3 gap-5 mt-12">
-        <div className="col-span-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-12">
+        <div className="col-span-2 relative">
           <img
             src={data.image}
             alt="image"
-            className="w-full h-[550px] rounded-2xl"
+            className="w-full h-[300px] md:h-[400px] lg:h-[550px] rounded-2xl"
           />
+          {data.discount ? (
+            <div className="absolute top-10 right-10 bg-[#00000041] backdrop-blur-md px-8 py-3 rounded-tl-3xl rounded-bl-3xl rounded-tr-3xl text-gray-100 font-medium text-lg">
+              {data.discount}% OFF
+            </div>
+          ) : (
+            <div></div>
+          )}
           <h2 className="mt-10 text-2xl font-semibold text-gray-700">
             {data.title}
           </h2>
@@ -26,15 +36,43 @@ const Details = () => {
               <p></p>
             )}
           </div>
-          <div className="grid grid-cols-3 text-lg">
-            <div className="flex items-center gap-3 font-medium">
-              <p>Adult : {data.adult}</p>
-              <p>Child : {data.child}</p>
+          <div className="grid grid-cols-1 gap-2 text-lg text-gray-900 font-medium">
+            <div className="flex items-center gap-3">
+              <p>
+                Adult : <span className="text-gray-500">{data.adult}</span>
+              </p>
+              <p>
+                Child : <span className="text-gray-500">{data.child}</span>
+              </p>
             </div>
-            <div>
-              <h2 className="text-gray-800 font-medium">Reviews : {data.reviews}</h2>
+            <div className="flex items-center gap-1">
+              <h2>
+                Reviews :{" "}
+                <span className="text-gray-500">({data.reviews})</span>
+              </h2>
+              <div className="flex items-center justify-center gap-1">
+                <img src={star} alt="image" className="w-5 h-5" />
+                <img src={star} alt="image" className="w-5 h-5" />
+                <img src={star} alt="image" className="w-5 h-5" />
+                <img src={star} alt="image" className="w-5 h-5" />
+                <img src={star} alt="image" className="w-5 h-5" />
+              </div>
             </div>
+            <h2>
+              Room size :{" "}
+              <span className="text-gray-500">({data.room_size})</span>
+            </h2>
           </div>
+          <p className="text-gray-500 mt-6 pr-2">{data.description}</p>
+          {user ? (
+            <button className="bg-red-500 hover:bg-green-400 hover:text-gray-700 transition-all duration-500 py-2 px-8 text-xl text-white font-medium mt-8">
+              Book Now
+            </button>
+          ) : (
+            <button className="bg-red-500 py-2 px-8 text-xl text-white font-medium mt-8">
+              <Link to="/login">Book Now</Link>
+            </button>
+          )}
         </div>
         <div className="mt-8">
           <h2 className="text-2xl font-medium">Contact</h2>
