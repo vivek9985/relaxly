204d2777f861ee46f1b68952ee87c0ac5ad0e3a6
vivek9985 <vivekanando363@gmail.com>
2023-11-08 16:48:47 +0600

    added bookings file and modified some files

---
+++ src/pages/MyBookings/Bookings.jsx
@@ -0,0 +1,37 @@
+const Bookings = ({ booking }) => {
+  console.log(booking);
+  const { roomImage, roomName, checkIn, checkOut, name, profile } =
+    booking || {};
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-10 border border-red-300 rounded-2xl">
+      <div className="m-4">
+        <h2 className="text-xl mb-2 font-semibold">{roomName}</h2>
+        <img
+          src={roomImage}
+          alt="image"
+          className="w-full h-[300px] md:h-[200px] lg:h-[230px] rounded-2xl"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-5 md:gap-7 mt-6 md:mt-0">
+        <h2 className="text-lg font-medium">
+          {" "}
+          Check In : <span className="text-red-500">{checkIn}</span>
+        </h2>
+        <h2 className="text-lg font-medium">
+          {" "}
+          Check Out : <span className="text-red-500">{checkIn}</span>
+        </h2>
+      </div>
+      <div className="flex lg:flex-col items-start lg:items-center justify-center gap-5 mt-6 md:mt-6 mb-8">
+        <button className="text-white bg-green-500 px-5 py-2 rounded-full">
+          Update Date
+        </button>
+        <button className="text-white bg-red-500 px-5 py-2 rounded-full">
+          Delete
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Bookings;
--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -1,7 +1,27 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Authprovider/Authprovider";
+import Bookings from "./Bookings";
+
 const MyBookings = () => {
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/bookings?email=${user?.email}`)
+    .then((res) =>res.json())
+    .then((data)=> setBookings(data))
+  }, []);
   return (
     <section className="w-[85%] mx-auto">
-      <h2> bookings</h2>
+      <h2 className="text-5xl font-semibold text-center my-14">
+        My <span className="text-red-500">Bookings</span>
+      </h2>
+      <div className="mb-32">
+        {
+          bookings.map((booking)=>(
+            <Bookings key={booking._id} booking={booking}></Bookings>
+          ))
+        }
+      </div>
     </section>
   );
 };
